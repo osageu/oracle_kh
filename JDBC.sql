fdd1cce899b4ff697bd4cc00f2301b558727c6ef
osageu <65025947+osageu@users.noreply.github.com>
2020-07-28 13:47:26 +0900

    ver 20.07.28_1.0.0

--- JDBC.sql
+++ JDBC.sql
@@ -1,11 +1,10 @@
 DROP TABLE MEMBER;
-
 CREATE TABLE MEMBER(
-    USERNO NUMBER PRIMARY KEY,
-    USERID VARCHAR2(15) NOT NULL UNIQUE,
-    USERPWD VARCHAR2(20) NOT NULL,
-    USERNAME VARCHAR2(20) NOT NULL,
-    GENDER CHAR(1) CHECK(GENDER IN ('M', 'F')),
+    USER_NO NUMBER PRIMARY KEY,
+    USER_ID VARCHAR2(15) NOT NULL UNIQUE,
+    USER_PW VARCHAR2(20) NOT NULL,
+    USER_NAME VARCHAR2(20) NOT NULL,
+    GENDER CHAR(1) CHECK (GENDER IN ('M', 'F')),
     AGE NUMBER,
     EMAIL VARCHAR2(30),
     PHONE CHAR(11),
@@ -14,21 +13,43 @@ CREATE TABLE MEMBER(
     ENROLLDATE DATE DEFAULT SYSDATE
 );
 
-SELECT * FROM MEMBER;
-
-DROP SEQUENCE SEQ_USER_NO;
 CREATE SEQUENCE SEQ_USER_NO;
-
+DROP SEQUENCE SEQ_USER_NO;
+DROP TABLE MEMBER;
 INSERT INTO MEMBER
-VALUES(SEQ_USER_NO.NEXTVAL, 'admin', '1234', '������', 'M', 45, 'admin@naver.com', 
-       '01012341111', '����� ���Ǳ�', '���ڱ�', '2020/06/21');
-
+VALUES (SEQ_USER_NO.NEXTVAL, 'ADMIN', '1234', '홍길동', 'M', 45, 'ADMIN@NAVER.COM', '01011111111', 'GANGNAM', 'SLEEP', '2020/06/21');
 INSERT INTO MEMBER
-VALUES(SEQ_USER_NO.NEXTVAL, 'user01', 'pass01', 'ȫ�浿', 'F', 20, NULL, 
-       '01011112222', NULL, '��ȭ����', '2020/07/01');
-       
-SELECT * FROM MEMBER;
-SELECT SEQ_USER_NO.NEXTVAL FROM DUAL;
-COMMIT;
+VALUES (SEQ_USER_NO.NEXTVAL, 'USER01', 'PASS01', '여자임', 'F', 20, 'USER01@NAVER.COM', '01022222222', 'HOEGI', 'FLOWER', '2020/07/01');
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 사용자가 회원가입 요청 시 실행해야 되는 sql문 (Statement)
+INSERT INTO MEMBER
+VALUES (SEQ_USER_NO.NEXTVAL, '???', '???', '???', '?', ?, '???', '???', '???', '???', '????/??/??');
+
+-- 사용자가 회원가입 요청 시 실행해야 되는 sql문 (PreparedStatement)
+INSERT INTO MEMBER
+VALUES (SEQ_USER_NO.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, SYSDATE);
+
+-- 사용자가 전체 회원 조회 요청 시 실행해야되는 sql문
+SELECT *
+FROM MEMBER;
+
+ALTER TABLE MEMBER ADD DEL_FLAG CHAR(1) DEFAULT 'N';
+SELECT * FROM MEMBER WHERE USER_ID = 'user02' AND USER_PW = 'pass02' AND DEL_FLAG = 'N';
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
